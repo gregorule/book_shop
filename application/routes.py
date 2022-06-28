8d7a57d8fae7b9cd2dcf60a314bd9507ec1dd438
gregorule <gregorule8@gmail.com>
2022-06-28 11:49:49 +0000

    update review

--- application/routes.py
+++ application/routes.py
@@ -34,6 +34,21 @@ def addBooks():
             return redirect(url_for('readBooks'))
     return render_template('addbook.html', form=form)
 
+@app.route('/addReview', methods=['GET','POST'])
+def addReview():
+    form = ReviewForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            reviewData = Reviews(
+                book_id = form.book_id.data,
+                rating = form.rating.data,
+                review = form.review.data
+            )
+            db.session.add(reviewData)
+            db.session.commit()
+            return redirect(url_for('readReviews'))
+    return render_template('addreview.html', form=form)
+
 @app.route('/update/<int:id>', methods= ['GET', 'POST'])
 def updateBooks(id):
     form = BookForm()
@@ -51,6 +66,22 @@ def updateBooks(id):
         form.pages.data = book.pages
         form.genre.data = book.genre
     return render_template('updateBooks.html', form=form)
+
+@app.route('/updateReview/<int:id>', methods= ['GET', 'POST'])
+def updateReviews(id):
+    form = ReviewForm()
+    review = Reviews.query.get(id)
+    if form.validate_on_submit():
+        review.book_id = form.book_id.data
+        review.rating = form.rating.data
+        review.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('readReviews'))
+    elif request.method == 'GET':
+        form.book_id.data = review.book_id
+        form.rating.data = review.rating
+        form.review.data = review.review
+    return render_template('updateReviews.html', form=form)
 
 @app.route('/delete/<int:id>')
 def deleteBooks(id):
